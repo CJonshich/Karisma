809b481fbcef16e4c1b6b3a9835a20c37caf8420
Cristian Bolije Jonshich <cristian_m34@hotmail.com>
2015-07-12 02:26:08 -0500

    AJAX y Alertify
    Agregado servicio en AJAX y modificado el mensaje de confirmación, ahora
    emplea Alertify

--- Karisma/WebContent/static/js/general.js
+++ Karisma/WebContent/static/js/general.js
@@ -6,5 +6,38 @@ $(document).ready(function(){
 	
 	$('.botonCSS').click(function(){
 		$('#opcion').val('agregar');
-	})
+	});
+	
+	$('.editServicio').click(function(){
+		var id = $(this).data("id");
+		var opcion = 'editar';
+		$.ajax({
+			url:'servicio?opcion='+opcion+'&id='+id,
+			type:'GET',
+			contentType:'application/json',
+			success: function(data){
+				$('#opcion').val('editar');
+				$('#txtCodservicio').val(data.idservicio);
+				$('#txtNombre').val(data.nomservicio);
+				$('#txaDescripcion').val(data.descripcion);
+				$('#txtCosto').val(data.costo);
+			},
+			error: function(){
+				alert('Ocurrio un error');
+			}
+		});
+	});
+	
+	$('.deleteServicio').click(function(event){
+		var name = $(this).data('name');
+		event.preventDefault();
+		alertify.defaults.glossary.title='Eliminar Servicio';
+		alertify.defaults.glossary.cancel = 'Cancelar';
+		alertify.defaults.theme.ok = "btn btn-success";
+		alertify.defaults.theme.cancel = "btn btn-danger";
+		alertify.confirm('¿Desea eliminar el servicio '+name+' ?',function(){
+			window.location.href=event.currentTarget.getAttribute('href');
+		});
+	});
+	
 });
