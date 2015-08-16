774a072ef2f3407a83f1d7a3d989d6be9ee863e7
Cristian Bolije Jonshich <cristian_m34@hotmail.com>
2015-08-16 00:51:01 -0500

    Creada clase para la respuesta de servicios

---
+++ Karisma/src/pe/AA/com/Util/ResponseObject.java
@@ -0,0 +1,46 @@
+package pe.AA.com.Util;
+
+public class ResponseObject {
+	
+	public ResponseObject(){
+		
+	}
+	
+	private String url;
+	private String errorMessage;
+	private String successMessage;
+	private boolean success;
+	
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public String getSuccessMessage() {
+		return successMessage;
+	}
+
+	public void setSuccessMessage(String successMessage) {
+		this.successMessage = successMessage;
+	}
+	
+}
